ee2551c1a9a380b0d854bf78dcb6726ed4076683
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-02-13 17:55:07 -0500

    Added teachers and students get request

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -2,13 +2,32 @@ const express = require('express');
 // const helpers = require('../db/dbHelper/helpers.js');
 
 const router = express.Router();
+const responseStatus = require('./responseStatus');
 
 // gets
 router.get('/', async (req, res) => {
   try {
-    res.status(200).json('Sanity Check');
+    res.status(responseStatus.success).json('Sanity Check');
   } catch (err) {
-    res.status(500).json('Error');
+    res.status(responseStatus.serverError).json('Error');
+  }
+});
+
+router.get('/teachers', async (req, res) => {
+  try {
+    const teachers = db('teachers');
+    res.status(responseStatus.success).json(teachers);
+  } catch (err) {
+    res.status(responseStatus.serverError).json('Error');
+  }
+});
+
+router.get('/students', async (req, res) => {
+  try {
+    const students = db('teachers');
+    res.status(responseStatus.success).json(students);
+  } catch (err) {
+    res.status(responseStatus.serverError).json('Error');
   }
 });
 
