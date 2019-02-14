48e35548720d6c1327e6922ba07814d57adfdfa8
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-02-14 10:22:02 -0500

    Updated responseStatus in routes

--- server/routes/classesRoutes.js
+++ server/routes/classesRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-const responseStatus = require('./responseStatus');
+const responseStatus = require('../config/responseStatusConfig');
 
 router.get('/', async (req, res) => {
   try {
