80e9bc2df7b9e0edb611fd8a00b5397e6459a03c
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-02-14 13:59:21 -0500

    Updated server file with new routes

--- server/api/server.js
+++ server/api/server.js
@@ -5,7 +5,15 @@ const server = express();
 configureMiddleware(server);
 
 const teachersRoutes = require('../routes/teachersRoutes');
+const studentsRoutes = require('../routes/studentsRoutes');
+const classesRoutes = require('../routes/classesRoutes');
+const questionsRoutes = require('../routes/questionsRoutes');
+const refreshrRoutes = require('../routes/refreshrRoutes');
 
+server.use('/classes', classesRoutes);
 server.use('/teachers', teachersRoutes);
+server.use('/students', studentsRoutes);
+server.use('/questions', questionsRoutes);
+server.use('/refreshr', refreshrRoutes);
 
 module.exports = server;
