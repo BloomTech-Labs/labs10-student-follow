ce8800898650d8ac2128d0d5c2d00368ac9eb91e
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-02-14 12:35:21 -0500

    Updated server line in index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const server = require('../backend/server/api/server');
+const server = require('../server/api/server');
 const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
