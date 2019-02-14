9c7f8f9008f468855d1d147680d88971668bdc38
szincone <sawyerzincone@gmail.com>
2019-02-14 11:18:53 -0700

    connect heroku w/ netlify :fire:

--- client/src/containers/LandingPage.js
+++ client/src/containers/LandingPage.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function LandingPage(props) {
   return (
     <>
-      {props.people.map((person) => (
-        <div key={person.name}>
-          <h1>{person.name}</h1>
+      {props.people.map((people) => (
+        <div key={people.id}>
+          <h1>{people.firstname}</h1>
         </div>
       ))}
     </>
