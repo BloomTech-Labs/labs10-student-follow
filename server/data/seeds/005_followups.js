cb30f922638db71b883367102a24d45fed54940b
Justin <jdlow2@gmail.com>
2019-02-14 22:06:38 -0500

    Finished seeds for all tables
    Also made 'truncate_and_seed.js' to truncate all tables and seed them
    for now. Will update heroku db tomorrow.

--- server/data/seeds/005_followups.js
+++ server/data/seeds/005_followups.js
@@ -5,15 +5,15 @@ const followups = [];
 for (let i = 0; i < 100; i++) {
   followups.push({
     date: faker.date.future(),
-    question: Math.floor(Math.random() * 100),
+    question: Math.ceil(Math.random() * 100),
   })
 }
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('followups').del()
+  return knex('refreshrs').del()
     .then(function () {
       // Inserts seed entries
-      return knex('followups').insert(followups);
+      return knex('refreshrs').insert(followups);
     });
 };
---
+++ server/data/seeds/007_classes_teachers.js
@@ -0,0 +1,33 @@
+
+const faker = require('faker');
+
+const classes_teachers = [];
+const t_ids = [];
+const c_ids = [];
+
+for (let i = 1; i < 401; i++) {
+  t_ids.push(i);
+  c_ids.push(i);
+}
+
+for (let i = 0; i < 400; i++) {
+  const [class_id] = [...c_ids.splice(Math.floor(Math.random() * c_ids.length), 1)];
+  const [ teacher_id ] = [...t_ids.splice(Math.floor(Math.random() * t_ids.length), 1)];
+  classes_teachers.push({
+    class_id: class_id,
+    teacher_id: teacher_id, 
+    refreshr_1: Math.ceil(Math.random() * 100),
+    refreshr_2: Math.ceil(Math.random() * 100),
+    refreshr_3: Math.ceil(Math.random() * 100),
+  })
+  
+}
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('classes_teachers').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('classes_teachers').insert(classes_teachers);
+    });
+};
