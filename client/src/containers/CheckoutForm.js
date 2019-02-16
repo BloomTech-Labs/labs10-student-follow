0c88f5e677e8a709c8f16e67934dc476cfa1518c
Justin <jdlow2@gmail.com>
2019-02-16 10:34:01 -0500

    Front end sends Stripe token to back end

--- client/src/containers/CheckoutForm.js
+++ client/src/containers/CheckoutForm.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { CardElement, injectStripe } from 'react-stripe-elements';
+import axios from 'axios';
+
+const url = 'http://localhost:9000/billing/charge';
 
 const CheckoutForm = props => {
   const handleSubmit = async e => {
-    const { token } = await props.stripe.createToken({ name: 'testName' });
-    console.log(token);
-    console.log(props.subType);
+    const { token } = await props.stripe.createToken({ name: 'Name' });
+    axios.post(url, { token: token.id, subType: props.subType }).then(res => {
+      if (res.status === 200) {
+        console.log('response', res.status);
+        console.log('purchase complete!');
+      }
+      else {
+        console.log('purchase failed');
+      }
+    })
+      .catch(err => console.log());
   };
 
   return (
--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -8,16 +8,22 @@ router.get('/', (req, res) => {
 });
 
 router.post('/charge', async (req, res) => {
+  console.log('req', req.body);
+  
   try {
     let {status} = await stripe.charges.create({
-      amount: 10,
+      amount: req.body.subType === 'monthly' ? 999 : 2999,
       currency: 'usd',
       description: 'test stripe charge',
-      source: req.body
+      source: req.body.token
     });
+    console.log('status', status);
+    
 
     res.json({status});
   } catch(err) {
+    console.log(err);
+    
     res.status(500).json({ error: `there was an error processing the payment: ${err}` })
   }
 
