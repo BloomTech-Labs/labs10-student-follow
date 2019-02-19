b4cf1a6204cf119b571a9ecf3d94d285e49372be
Chaya Otikor <c.otikor@gmail.com>
2019-02-19 15:07:18 -0700

    Folder structure updated, didn't touch ClassView

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,26 +1,38 @@
 /* NAV */
-import Navbar from "./navigation/Navbar";
-import Navcrumbs from "./navigation/Navcrumbs";
+import Navbar from './navigation/Navbar';
+import Navcrumbs from './navigation/Navcrumbs';
 
 /* AUTH */
 import Auth from './authentication/Auth';
 import Login from './authentication/Login';
 
 /* VIEWS */
-import RefreshrView from "./views/RefreshrView";
-import LandingPage from './views/LandingPage'
-import LoadingPage from './views/LoadingPage';
 import ClassView from './views/ClassView';
+import LandingPage from './views/LandingPage';
+import LoadingPage from './views/LoadingPage';
 
 /* BILLING */
-import BillingPage from './billing/BillingPage'
-import CheckoutForm from './billing/CheckoutForm'
+import BillingPage from './billing/BillingPage';
+import CheckoutForm from './billing/CheckoutForm';
 
 /* REFRESHRS */
-import RefreshrList from "./refreshrs/RefreshrList";
-import RefreshrCard from "./refreshrs/RefreshrCard";
+import RefreshrList from './refreshrs/RefreshrList';
+import RefreshrCard from './refreshrs/RefreshrCard';
 
 /* CLASSES */
 import ClassList from './classes/ClassList';
 
-export {  Navcrumbs, Navbar, RefreshrList, RefreshrCard, RefreshrView, LandingPage, LoadingPage, Auth, Login ,BillingPage, CheckoutForm, ClassView, ClassList };
+export {
+  Navcrumbs,
+  Navbar,
+  RefreshrList,
+  RefreshrCard,
+  ClassList,
+  ClassView,
+  LandingPage,
+  LoadingPage,
+  Auth,
+  Login,
+  BillingPage,
+  CheckoutForm
+};
