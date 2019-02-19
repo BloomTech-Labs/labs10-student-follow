95e603a49b291ce8bfda8ce326c37bf4ff463b1b
Timothy Hoang <timh1203@gmail.com>
2019-02-19 11:08:02 -0600

    Add axios call for list GET and update example_env_file

--- client/src/components/ClassView.js
+++ client/src/components/ClassView.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import axios from 'axios';
 import { withStyles } from "@material-ui/core/styles";
 import { Navcrumbs, Navbar } from "./index.js";
 
@@ -9,7 +10,18 @@ const styles = theme => ({
   }
 });
 
-const ClassView = props => {
+function ClassView(props) {
+  const getAllLists = () => {
+    const url = "https://api.sendgrid.com/v3/contactdb/lists"
+    const headers = {
+      headers: { "Authorization": `Bearer ${process.env.REACT_APP_SENDGRID_API_KEY}` }
+    }
+
+    axios.get(url, headers)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
+
   return (
     <>
       <Navcrumbs />
@@ -19,6 +31,7 @@ const ClassView = props => {
         </Grid>
         <Grid item xs={10}>
           <p>ClassView</p>
+          <button onClick={getAllLists}>getAllLists</button>
         </Grid>
       </Grid>
     </>
