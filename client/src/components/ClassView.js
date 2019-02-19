99ea269d3573cffa04c48ca55aa8496618c098f8
Timothy Hoang <timh1203@gmail.com>
2019-02-19 12:10:06 -0600

    Add basic CRUD operations for Lists

--- client/src/components/ClassView.js
+++ client/src/components/ClassView.js
@@ -11,14 +11,67 @@ const styles = theme => ({
 });
 
 function ClassView(props) {
+  // Variables used in multiple operations
+  const headers = {
+    "headers": { "Authorization": `Bearer ${process.env.REACT_APP_SENDGRID_API_KEY}` },
+    "content-type": "application/json"
+  }
+  const listId = '7061244'
+
+  // Basic CRUD operations
+  const createList = () => {
+    const url = "https://api.sendgrid.com/v3/contactdb/lists"
+    const body = {
+      "name": "test5"
+    }
+    axios.post(url, body, headers)
+      .then(res => {
+        console.log(`===Create list: ${res.data.name}===`)
+        console.log(res.data.id)
+      })
+      .catch(err => console.log(err))
+  }
+
   const getAllLists = () => {
     const url = "https://api.sendgrid.com/v3/contactdb/lists"
-    const headers = {
-      headers: { "Authorization": `Bearer ${process.env.REACT_APP_SENDGRID_API_KEY}` }
-    }
-
     axios.get(url, headers)
-      .then(res => console.log(res))
+      .then(res => {
+        console.log(`===Retrieve list: all===`)
+        console.log(res.data.lists)
+      })
+      .catch(err => console.log(err))
+  }
+
+  const getOneList = () => {
+    const url = `https://api.sendgrid.com/v3/contactdb/lists/${listId}`
+    axios.get(url, headers)
+      .then(res => {
+        console.log(`===Retrieve list: ${res.data.name}===`)
+        console.log(res.data)
+      })
+      .catch(err => console.log(err))
+  }
+
+  const updateOneList = () => {
+    const url = `https://api.sendgrid.com/v3/contactdb/lists/${listId}`
+    const body = {
+      "name": "modifiedListName"
+    }
+    axios.patch(url, body, headers)
+      .then(res => {
+        console.log(`===Update list: ${res.data.name}===`)
+        console.log(res)
+      })
+      .catch(err => console.log(err))
+  }
+
+  const deleteOneList = () => {
+    const url = `https://api.sendgrid.com/v3/contactdb/lists/${listId}?delete_contacts=true`
+    axios.delete(url, headers)
+      .then(res => {
+        console.log(`===Delete list: ${res.statusText}===`)
+        console.log(res)
+      })
       .catch(err => console.log(err))
   }
 
@@ -31,7 +84,13 @@ function ClassView(props) {
         </Grid>
         <Grid item xs={10}>
           <p>ClassView</p>
-          <button onClick={getAllLists}>getAllLists</button>
+          <div>
+            <button onClick={createList}>createList</button>
+            <button onClick={getAllLists}>getAllLists</button>
+            <button onClick={getOneList}>getOneList</button>
+            <button onClick={updateOneList}>updateOneList</button>
+            <button onClick={deleteOneList}>deleteOneList</button>
+          </div>
         </Grid>
       </Grid>
     </>
