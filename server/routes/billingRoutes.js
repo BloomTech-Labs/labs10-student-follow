296ef2551d517de1ec2e6335716cf34252f51000
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-02-19 17:22:28 -0500

    Created a product, plan and successful subscription payment to test plan

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const stripe = require('stripe')(process.env.REACT_APP_SECRET_KEY); // secret test key in .env
+const stripe = require('stripe')(process.env.SECRET_KEY); // secret test key in .env
 
 const router = express.Router();
 
@@ -8,21 +8,62 @@ router.get('/', (req, res) => {
 });
 
 router.post('/charge', async (req, res) => {
-  const user = req.body.token;
+  const user = req.body;
+  console.log('USER', user.token.id);
   try {
-    let { status } = await stripe.charges.create({
-      amount: req.body.subType,
-      currency: 'usd',
-      description: 'test stripe charge',
-      email: user.card.email,
-      statement_descriptor: 'Refreshr Payment',
-      source: user.id
+    // let { status } = await stripe.charges.create({
+    //   amount: req.body.subType,
+    //   currency: 'usd',
+    //   description: 'test stripe charge',
+    //   email: user.email,
+    //   statement_descriptor: 'Refreshr Payment',
+    //   source: user.token.id
+    // });
+    // res.json({ status });
+
+    const product = stripe.products.create({
+      name: 'Monthly Subscription',
+      type: 'service'
     });
-    res.json({ status });
+
+    const plan = stripe.plans.create({
+      currency: 'usd',
+      interval: 'month',
+      product: 'prod_EYoWV6h9SKDkgV',
+      nickname: 'Monthly Plan',
+      amount: 999 // cents
+    });
+
+    console.log('PLAN', plan);
+
+    const sub = stripe.subscriptions.create(
+      {
+        // customer: 'cus_ETpOayu8argnJO',
+        customer: 'cus_EYn0mQnCLmD55u', // nick oferrall
+        // amount: user.subType,
+        // currency: 'usd',
+        // description: 'test stripe charge',
+        // // email: user.card.email,
+        // statement_descriptor: 'Refreshr Payment',
+        // source: user.id,
+        items: [
+          {
+            plan: 'ruby-infinite-345'
+            // plan: 'prod_EYoWV6h9SKDkgV'
+          }
+        ]
+      },
+      function(err, subscription) {
+        // asynchronously called
+      }
+    );
+    // console.log('SUB', sub);
+    res.json({ sub });
   } catch (err) {
-    res
-      .status(500)
-      .json({ error: `There was an error processing the payment: ${err}` });
+    console.log('ERROR', err);
+    // res
+    //   .status(500)
+    //   .json({ error: `There was an error processing the payment: ${err}` });
   }
 });
 
