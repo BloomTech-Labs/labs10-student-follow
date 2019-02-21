9fe5041c5468976e8a8e53e34821690521523079
szincone <sawyerzincone@gmail.com>
2019-02-21 15:15:58 -0700

    refactored to use material-ui components - very basic styling, will refactor when I see the style everyone wants to use

--- client/src/components/classes/RecipientForm.js
+++ client/src/components/classes/RecipientForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
   wrapper: {
@@ -17,41 +19,45 @@ function RecipientForm(props) {
     e.preventDefault()
     props.setRecipientForm((!props.onRecipientForm))
   }
-
+  
   return (
     <Grid className={props.classes.wrapper}>
       <p>RecipientForm Component</p>
-      <button onClick={(e) => handleSubmit(e)}>COMPLETE</button>
+      <Button onClick={(e) => handleSubmit(e)}>COMPLETE</Button>
       <form className={props.classes.form} onSubmit={(e) => handleSubmit(e)}>
-        <input
+        <TextField
           name="contactName"
           type="text"
+          variant="outlined"
           value={contactName}
           placeholder="Add Contact Name"
           onChange={(e) => addContactName(e.target.value)}
         />
-        <input
+        <TextField
           name="contactsName"
           type="text"
+          variant="outlined"
           value={contactsName}
           placeholder="Add Contacts Names"
           onChange={(e) => addContactsName(e.target.value)}
         />
-        <input
+        <TextField
           name="contacts"
           type="text"
+          variant="outlined"
           value={contacts}
           placeholder="Find Contact Name"
           onChange={(e) => getContacts(e.target.value)}
         />
-        <input
+        <TextField
           name="contact"
           type="text"
+          variant="outlined"
           value={contact}
           placeholder="Find Contact Name"
           onChange={(e) => deleteContact(e.target.value)}
         />
-        <button type="submit">Next</button>
+        <Button variant="outlined" color="secondary" type="submit">Next</Button>
       </form>
     </Grid>
   );
