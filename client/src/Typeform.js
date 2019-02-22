fc002e19526af71b8164bb0755dc520b192d9d5d
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-02-22 12:58:08 -0500

    Embedded typeform

---
+++ client/src/Typeform.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ReactTypeformEmbed } from 'react-typeform-embed';
+
+const Typeform = () => {
+  return (
+    <div>
+      <h1>My Typeform</h1>
+      <ReactTypeformEmbed url="https://nick971045.typeform.com/to/eaHFcw/" />
+    </div>
+  );
+};
+
+export default Typeform;
