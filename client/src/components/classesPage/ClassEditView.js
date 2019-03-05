4c6d466dfb648c7e566a749026b0a6a6821dc001
Justin <jdlow2@gmail.com>
2019-03-05 14:17:19 -0500

    Styles for student list and add input fields

--- client/src/components/classesPage/ClassEditView.js
+++ client/src/components/classesPage/ClassEditView.js
@@ -6,7 +6,9 @@ import {
   Button,
   Typography,
   CardContent,
-  Icon
+  Icon,
+  Paper,
+  TextField
 } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -15,16 +17,28 @@ import axios from 'axios';
 
 const styles = theme => ({
   wrapper: {
-    color: 'white',
-    marginTop: 50
+    marginTop: theme.spacing.unit * 6,
+    display: 'flex',
+    flexDirection: 'column',
+    border: `1px solid ${theme.palette.secondary.main}`,
+    ...theme.mixins.gutters(),
+    alignItems: 'center',
+    color: theme.palette.primary.contrastText,
+    background: theme.palette.primary.dark,
+    [theme.breakpoints.down('sm')]: {
+      width: '80%'
+    },
+    [theme.breakpoints.only('md')]: {
+      width: '60%'
+    }
   },
   refreshrList: {
     display: 'flex',
     flexWrap: 'wrap',
-    border: '1px solid black'
+    border: `1px solid ${theme.palette.secondary.main}`,
+    padding: theme.spacing.unit * 8
   },
   refreshrCard: {
-    width: '25%',
     height: 200,
     border: '1px solid white',
     margin: theme.spacing.unit * 3,
@@ -32,10 +46,10 @@ const styles = theme => ({
   },
   studentList: {
     display: 'flex',
-    flexDirection: 'column',
-    border: '1px solid white',
+    flexFlow: 'column wrap',
+    border: `1px solid ${theme.palette.secondary.main}`,
     flexWrap: 'wrap',
-    height: '30vh'
+    padding: theme.spacing.unit * 6
   },
   buttonBox: {
     height: 50
@@ -45,6 +59,19 @@ const styles = theme => ({
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -40%)'
+  },
+  title: {
+    color: `${theme.palette.primary.contrastText}`
+  },
+  settingsBox: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: theme.spacing.unit * 2
+  },
+  inputs: {
+    color: theme.palette.primary.main,
+    backgroundColor: theme.palette.secondary.main
   }
 });
 
@@ -52,6 +79,7 @@ function ClassEditView(props) {
   const { classes } = props;
   const classId = props.match.params.id;
   const token = localStorage.getItem('accessToken');
+  const userID = localStorage.getItem('user_id');
   const ax = axios.create({
     baseURL: 'http://localhost:9000', // development
     headers: {
@@ -71,6 +99,7 @@ function ClassEditView(props) {
     // fetchRefreshrs();
     // fetchTeacherRefreshrs();
     fetchClass();
+    fetchTeacherRefreshrs();
   }, []);
 
   /*
@@ -101,10 +130,10 @@ function ClassEditView(props) {
     setRefreshrs(res.data.specifiedClass.refreshrs);
   }
 
-  async function fetchStudents() {
-    const res = await ax.get(`/classes/${classId}/students`);
-    setStudents(res.data);
-  }
+  // async function fetchStudents() {
+  //   const res = await ax.get(`/classes/${classId}/students`);
+  //   setStudents(res.data);
+  // }
 
   async function fetchRefreshrs() {
     const res = await ax.get(`/classes/${classId}/refreshrs`);
@@ -113,7 +142,7 @@ function ClassEditView(props) {
 
   async function fetchTeacherRefreshrs(id) {
     // this should be user id, not 35
-    const res = await ax.get('/refreshrs/teachers/35');
+    const res = await ax.get(`/refreshrs/teachers/${userID}`);
     const unassignedRefreshrs = res.data.filter(r => !refreshrs.includes(r)); // unsure if this filter will work, need to test
     setTeacherRefs(unassignedRefreshrs);
   }
@@ -146,23 +175,65 @@ function ClassEditView(props) {
     setModalIsOpen(false);
   }
 
+  const [newStudent, setNewStudent] = useState({
+    first_name: '',
+    last_name: '',
+    email: ''
+  });
+
+  useEffect(() => {
+    console.log(newStudent);
+  }, [newStudent]);
+
+  const handleChange = e => {
+    console.log(e);
+
+    setNewStudent({
+      ...newStudent,
+      [e.target.name]: e.target.value
+    });
+  };
+
   async function dropStudents() {
     const res = await ax.post(`/classes/${classId}/drop/`, {
       students: selectedStudents
     });
     console.log('dropped:', res);
     setSelectedStudents([]);
-    fetchStudents(); // better way to do this than calling this here?
+    // fetchStudents(); // better way to do this than calling this here?
   }
+
+  const makeInput = (name, label) => {
+    return (
+      <TextField
+        className={classes.inputs}
+        variant="outlined"
+        label={label}
+        onChange={handleChange}
+        name={name}
+      />
+    );
+  };
 
   return (
     <Grid className={props.classes.wrapper}>
-      <h1>Students</h1>
+      <Typography variant="h6" className={classes.title}>
+        Settings
+      </Typography>
+      <Grid classname={classes.settings}>
+        {makeInput('first_name', 'First Name')}
+        {makeInput('last_name', 'Last Name')}
+        {makeInput('email', 'Email')}
+      </Grid>
+      <Typography variant="h6" className={classes.title}>
+        Students
+      </Typography>
       <Grid className={classes.studentList}>
         {students.map(s => (
           <Grid key={s.student_id}>
             <span>{`${s.name}`}</span>
             <Checkbox
+              color="secondary"
               value={`${s.student_id}`}
               checked={selectedStudents.includes(s.student_id)}
               onClick={e => selectStudent(e)}
@@ -179,7 +250,9 @@ function ClassEditView(props) {
       </Grid>
 
       <Grid>
-        <h1>Refreshrs</h1>
+        <Typography variant="h6" className={classes.title}>
+          Refreshrs
+        </Typography>
         <Grid className={classes.refreshrList}>
           {refreshrs.map(r => (
             <Card className={classes.refreshrCard} key={r.refreshr_id} raised>
@@ -215,4 +288,4 @@ function ClassEditView(props) {
   );
 }
 
-export default withStyles(styles)(ClassEditView);
+export default withStyles(styles, { withTheme: true })(ClassEditView);
