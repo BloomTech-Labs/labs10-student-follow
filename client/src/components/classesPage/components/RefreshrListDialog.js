eb9d1ce1ac38ae2d317d72f59ff3bb4d8cab0716
Justin <jdlow2@gmail.com>
2019-03-14 16:17:41 -0400

    Styles text and border of refreshr dialog

--- client/src/components/classesPage/components/RefreshrListDialog.js
+++ client/src/components/classesPage/components/RefreshrListDialog.js
@@ -8,28 +8,38 @@ import {
   DialogTitle,
   Dialog
 } from '@material-ui/core/';
-import AddIcon from '@material-ui/icons/Add';
+import AddCircleOutline from '@material-ui/icons/AddCircleOutline';
 
-const styles = {
+const styles = theme => ({
+  dialog: {
+    border: `1px solid ${theme.palette.secondary.main}`
+  },
   listItem: {
-    color: 'blue',
-    width: '100vh',
-    height: '100vh',
-    backgroundColor: 'red'
+    color: theme.palette.secondary.main
+  },
+  title: {
+    color: theme.palette.secondary.main
   },
   link: {
     textDecoration: 'none'
+  },
+  icon: {
+    color: theme.palette.secondary.main
   }
-};
+});
 
 const RefreshrDialog = props => {
   const { classes } = props;
-  // console.log(props);
-
   return (
     <div>
-      <Dialog onClose={props.handleClose} open={props.open}>
-        <DialogTitle>Select a refreshr</DialogTitle>
+      <Dialog
+        classes={{ paper: classes.dialog }}
+        onClose={props.handleClose}
+        open={props.open}
+      >
+        <DialogTitle>
+          <span className={classes.title}>Select a refreshr</span>
+        </DialogTitle>
         <div>
           <List>
             {props.refreshrs.map(r => (
@@ -38,13 +48,19 @@ const RefreshrDialog = props => {
                 onClick={() => props.selectNewRefreshr(r.refreshr_id)}
                 key={r.refreshr_id}
               >
-                <ListItemText primary={r.name} />
+                <ListItemText
+                  classes={{ primary: classes.listItem }}
+                  primary={r.name}
+                />
               </ListItem>
             ))}
             <Link className={classes.link} to="/refreshrs/create">
               <ListItem>
-                <AddIcon />
-                <ListItemText primary={'Create a new refreshr'} />
+                <AddCircleOutline className={classes.icon} />
+                <ListItemText
+                  classes={{ primary: classes.listItem }}
+                  primary={'Create a new refreshr'}
+                />
               </ListItem>
             </Link>
           </List>
